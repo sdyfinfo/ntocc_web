f03899b41fec2976a8a6b0ae887df991b0d0d864
zhangxiaodang <961590849@qq.com>
2020-03-24 08:28:20 +0800

    修改测试地址

--- public/js/pages/const.js
+++ public/js/pages/const.js
@@ -3,12 +3,11 @@
  */
 
 //var hostIp = "http://127.0.0.1:";
-
 // var regulateSucc = {
 //     "uploadFolder" : "/home/yfdev/src/ywt_web",                       // 上传文件所在文件夹
-//     "loginUrl" : hostIp + "8001/gramtu/ac/web/",                      // 登录URL
-//     "userHostUrl" : hostIp + "8001/gramtu/ac/web/front/",             // 用户相关URL
-//     "gramtuWebUrl": hostIp + "8002/gramtu/web/"                       // web端URL
+//     "loginUrl" : hostIp + "8001/ntocc/ntocc-ac/web/",                     // 登录URL   "/gramtu/ac/web/"  /ntocc/ntocc-ac/web/
+//     "userHostUrl" : hostIp + "8001/ntocc/ntocc-ac/api/web/",              // 用户相关URL（无车运平台系统管理）   "/gramtu/ac/web/front/"  /ntocc/ntocc-ac/api/web/
+//     "businessUrl": hostIp + "8002/ntocc/ntocc-business/api/web/"            //无车运平台（基础数据管理）相关
 // };
 
 //var hostIp = "http://39.107.68.137";
